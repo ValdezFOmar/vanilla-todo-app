4015d94575e8b98df8f524f01952b8d594e35ca4
Omar Valdez <omarantoniovaldezf2@gmail.com>
2023-07-28 20:27:20 -0700

    Test script for generating TODOS

---
+++ scripts/tests/createDefaultTODOS.js
@@ -0,0 +1,18 @@
+import {TODOElement} from '../TODOElement.js'
+import {checkmarkButton, trashcanButton} from '../IconButton.js'
+
+const TODOsContainer = document.querySelector('.content')
+
+const description = `
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+Suspendisse finibus est at nunc elementum, non consectetur orci consectetur. 
+Donec in arcu dolor. Proin sit amet ex non diam fringilla efficitur.
+`
+
+for (let i = 0; i < 10; i++) {
+  const title = `Title ${i + 1}`
+  const newTODO = new TODOElement(title, description, false)
+  newTODO.addCheckButton(checkmarkButton.createButtonElement())
+  newTODO.addDeleteButton(trashcanButton.createButtonElement())
+  TODOsContainer.append(newTODO.getTODONode())
+}
